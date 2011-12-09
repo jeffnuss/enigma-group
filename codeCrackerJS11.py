295b76e38a7f4836b723e594d02ac59db13865a1
Jeff Nuss <jeffnuss@hotmail.com>
2011-12-09 11:33:20 -0700

    finished JS11 and the script seems pretty good to me; JS12 is an ugly piece of junk though, but it got the job done

--- codeCrackerJS11.py
+++ codeCrackerJS11.py
@@ -10,21 +10,28 @@
 #-------------------------------------------------------------------------------
 #!/usr/bin/env python
 
+## Recursive funtion to reverse the algorithm
 def codeCracker(n):
     i = 0.0
-
+    charList = "abcdefghijklmnopqrstuvwxyz"
     while i < 27:
         i += 1
-        test = (n - i) % 17
         if (n - i) == 0:
-            print("done")
-            return 0;
+            password.append(charList[int(i - 1)])
+            return 0
+        test = (n - i) % 17
         if test == 0:
-            print(i)
-            codeCracker((n - i) / 17.0)
-    return -1
+            newNumber = (n - i) / 17.0
+            password.append(charList[int(i - 1)])
+            if codeCracker(newNumber) == 0:
+                return 0
+    password.pop()
 
-
-
-
+password = []
+##codeCracker(53) ## cb
+##codeCracker(907) ## cbf
+##codeCracker(15421) ## cbfb
 codeCracker(248410397744610)
+
+password.reverse()
+print (''.join(password))
